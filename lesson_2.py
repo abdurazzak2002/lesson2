7a22eede23a0fd23d10d73885c73f84cf82a589c
abdurazzak2002 <toloev.abi97@gmail.com>
2021-09-18 13:43:17 +0600

    start

---
+++ lesson_2.py
@@ -0,0 +1,79 @@
+# text = 'Programming academy IT-Run'
+# print(text.replace('academy', 'school'))
+
+
+# text = 'Programming academy IT-Run'
+# print(text.split('academy'))
+
+# text = 'Programming academy IT-Run'
+# print(text.count(''))
+
+
+# fruit = 'banana'
+# print(fruit.count('a'))
+# name = input('Введите имя: ')
+# name = name.strip()
+# print('Привет ' + name + '!')
+
+
+# title = '-Вчера прошла открытие Олимпиалы 2021'
+# title = title.strip('-')
+# print(title)
+
+
+# num = '1234567'
+# print(num.isnumeric())
+# num = '1234567f'
+# print(num.isnumeric())
+
+
+# fruits = 'milk, apple, tomato, orange'
+# print(fruits.split(','))
+
+
+# name = 'Abdurazzak'
+# print(len(name))
+
+# sport = 'Football'
+# part = sport[1:4]
+# print(sport[0:5])
+# print(part)
+
+# name = "Jonh"
+# last = "Robert"
+# print(name[0])
+
+# numbers = '0123456789'
+# print(numbers[1::2])
+
+
+# text = '''
+#     aboifamsdsd,
+# asdfkmdsvdas
+# dvasdvods;
+# '''
+# print(text)
+
+
+
+### числа 
+# Integer - int - целое число 
+# print(85)
+# print(62*2)
+# print(50/2)
+# print(20-3)
+# print(12**2)#поднимает в степени
+# print(12/(78*8))
+
+#float  дробные числа 
+# print(100*0.1)
+# print(10.5+10.5)
+
+
+#возможная ошибка
+# age = 18
+# name = 'Abi'
+
+names = ['Abi', 'Beka', 'Juma', 'Oskar']
+names = names.append('asdfgh')
+print(names)
